06141f00d7c8e0ef223a165b33fa07b11a343324
Kryshtof Yurii <yura.krishtof@gmail.com>
2023-09-25 21:07:51 +0300

    Removed some hardcode. For now scrapper dynamically retrieves list of countries to visit, duration etc

--- src/org.chillnvibe/scrapper/main.py
+++ src/org.chillnvibe/scrapper/main.py
@@ -1,6 +1,7 @@
 import logging
+import time
 
-import traverser
+import retriever
 
 from selenium import webdriver
 from selenium.webdriver import ActionChains
@@ -9,19 +10,30 @@ url = "https://www.otpusk.ua/"
 driver = webdriver.Chrome()
 action = ActionChains(driver)
 
-try:
-    driver.get(url=url)
-    links = list(traverser.get_offer_links(driver=driver, action=action))
 
+def traverse_offer_links_from_search_container(from_country_item, to_country_item, days_item):
+    # driver.get(url=url)
+    # time.sleep(5)
+    links = list(retriever.get_offer_links(
+            driver=driver,
+            action_chain=ActionChains(driver),
+            from_country_item=from_country_item,
+            to_country_item=to_country_item,
+            duration_item=days_item))
     for a in links:
         try:
-            traverser.read_offer_info(driver, a)
+            retriever.read_offer_info(driver, a)
         except Exception as ex:
             logging.error(f'Cannot parsing the proposal by link: {a}\n{ex}')
-    # check_travel_info(links[0])
 
-except Exception as ex:
-    print(ex)
-finally:
-    driver.close()
-    driver.quit()
+
+if __name__ == '__main__':
+    driver.get(url=url)
+    from_country_elements_list = retriever.retrieve_from_countries_elements(driver=driver, action_chain=action)
+    to_country_elements_list = retriever.retrieve_to_countries_elements(driver=driver, action_chain=action)
+    duration_elements_list = retriever.retrieve_duration_elements(driver=driver, action_chain=action)
+
+    for from_country in from_country_elements_list:
+        for to_country in to_country_elements_list:
+            for duration in duration_elements_list:
+                traverse_offer_links_from_search_container(from_country, to_country, duration)
--- src/org.chillnvibe/scrapper/retriever.py
+++ src/org.chillnvibe/scrapper/retriever.py
@@ -11,6 +11,7 @@ logging.basicConfig(format='%(asctime)s %(name)s %(levelname)s\nINFO:%(message)s
                     filemode="w",
                     datefmt='%Y-%m-%d %H:%M:%S')
 
+
 def read_offer_info(driver, url):
     driver.get(url)
     util.wait_for_element_presence(driver, delay=10, by=By.CLASS_NAME, value='src-pages-Offer-styles__head')
@@ -60,21 +61,58 @@ def read_offer_info(driver, url):
     print()
 
 
-def get_offer_links(driver, action, from_country_id= 0, to_country_id= 0, days_id= 0):
+def retrieve_items_from_search_container_by_id(driver, action_chain, by, value):
+    util.wait_for_element_presence(driver, delay=10, by=By.ID,
+                                   value='otp_search_form')
+    search_container_element = driver.find_element(By.ID,
+                                         'otp_search_form')
+    downshift_element = search_container_element.find_element(by, value)
+    action_chain.click(downshift_element).perform()
+
+    return [a.get_attribute('id') for a in search_container_element.find_elements(By.TAG_NAME, 'li')]
+
+
+def retrieve_from_countries_elements(driver, action_chain):
+    return retrieve_items_from_search_container_by_id(driver, action_chain, By.ID, 'downshift-0-input')
+
+
+def retrieve_to_countries_elements(driver, action_chain):
+    return retrieve_items_from_search_container_by_id(driver, action_chain, By.ID, 'downshift-1-input')
+
+
+def retrieve_duration_elements(driver, action_chain):
+    return retrieve_items_from_search_container_by_id(driver, action_chain, By.CLASS_NAME, 'durationControlRoot')
+
+
+def get_offer_links(driver,
+                    action_chain,
+                    from_country_item,
+                    to_country_item,
+                    duration_item):
+    driver.get(url="https://www.otpusk.ua/")
+
+    time.sleep(3)
+    # util.wait_for_element_presence(driver, delay=5, by=By.ID,
+    #                                value='otp_search_form')
+
+    search_container_element = driver.find_element(By.ID, 'otp_search_form')
+
+    downshift_element = search_container_element.find_element(By.ID, 'downshift-0-input')
+    action_chain.click(downshift_element).perform()
+
+    select_from_country = driver.find_element(By.ID, from_country_item)
+    action_chain.click(select_from_country).perform()
+
+    select_to_country = driver.find_element(By.ID, to_country_item)
+    action_chain.click(select_to_country).perform()
+
+    select_duration = driver.find_element(By.ID, duration_item)
+
     filter_element = driver.find_element(By.CLASS_NAME,
                                          'src-containers-search-OtpuskInlineSearchForm-styles__submit')
     find_button_element = filter_element.find_element(By.TAG_NAME, 'button')
 
-    action.click(find_button_element).perform()
-
-    select_from_country = driver.find_element(By.ID, f'downshift-0-item-{from_country_id}')
-    action.click(select_from_country).perform()
-
-    select_to_country = driver.find_element(By.ID, f'downshift-1-item-{to_country_id}')
-    action.click(select_to_country).perform()
-
-    select_to_country = driver.find_element(By.ID, f'downshift-2-item-{days_id}')
-    action.click(select_to_country).click(find_button_element).perform()
+    action_chain.click(select_duration).click(find_button_element).perform()
 
     util.wait_for_element_presence(driver, 15, By.CLASS_NAME, 'src-components-result-Card-styles__root')
 
