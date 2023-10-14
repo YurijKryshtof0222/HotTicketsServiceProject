e621aab65ca3236108602340c88526bc8faea196
Kryshtof Yurii <yura.krishtof@gmail.com>
2023-10-14 17:49:59 +0300

    Added DbController.get_filtered_records_as_json method

--- src/restapi/db_controller.py
+++ src/restapi/db_controller.py
@@ -9,8 +9,7 @@ class DbController:
         self.cursor = self.conn.cursor()
         self.create_table()
 
-
-    #using for init data base
+    # using for init data base
     def create_table(self):
         # Створюємо таблицю, якщо вона не існує
         create_table_offer = '''
@@ -41,7 +40,37 @@ class DbController:
         self.cursor.execute(create_table_offer_links)
         self.conn.commit()
 
-    #add record (tour) to database
+    def __execute_query(self, query, limit, offset):
+        self.cursor.execute(query, (limit, offset))
+        rows = self.cursor.fetchall()
+        records = []
+        for row in rows:
+            offer_id = row[1]
+            query_links = '''
+                                       SELECT * FROM offer_links WHERE offer_id = ?;
+                                   '''
+            self.cursor.execute(query_links, (offer_id,))
+            links_row = self.cursor.fetchall()
+            links = []
+            for link in links_row:
+                links.append(link[2])
+            record = {
+                "offer_id": row[1],
+                "offer_name": row[2],
+                "offer_source": row[3],
+                "location": row[4],
+                "people_count": row[5],
+                "description": row[6],
+                "food_info": row[7],
+                "night_count": row[8],
+                "start_date": row[9],
+                "end_date": row[10],
+                "links": links
+            }
+            records.append(record)
+        return json.dumps(records)
+
+    # add record (tour) to database
     def add_data(self,
                  offer_id,
                  offer_name,
@@ -75,20 +104,20 @@ class DbController:
                             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
                         '''
         self.cursor.execute(insert_to_offer_sql, (
-                                                  None,
-                                                  offer_id,
-                                                  offer_name,
-                                                  offer_source,
-                                                  location,
-                                                  people_count,
-                                                  description,
-                                                  food_info,
-                                                  night_count,
-                                                  start_date,
-                                                  end_date,
-                                                  transport_info,
-                                                  price
-                                                  ))
+            None,
+            offer_id,
+            offer_name,
+            offer_source,
+            location,
+            people_count,
+            description,
+            food_info,
+            night_count,
+            start_date,
+            end_date,
+            transport_info,
+            price
+        ))
         self.conn.commit()
         for link in links:
             insert_to_offer_links_sql = '''
@@ -98,8 +127,7 @@ class DbController:
             self.cursor.execute(insert_to_offer_links_sql, (None, offer_id, link))
             self.conn.commit()
 
-
-    #select all record with pagination
+    # select all record with pagination
     def get_all_records_as_json(self, page, limit):
         # Calculate the offset based on the page and limit
         offset = (page - 1) * limit
@@ -138,6 +166,93 @@ class DbController:
             records.append(record)
 
         return json.dumps(records)
+
+    def get_filtered_records_as_json(self,
+                                     page,
+                                     limit,
+                                     offer_name='',
+                                     country='',
+                                     people_count_operation='',
+                                     people_count_value=0,
+                                     food_info='',
+                                     night_count_operation='',
+                                     night_count_value=0,
+                                     start_date_operation='',
+                                     start_date_value=0,
+                                     end_date_operation='',
+                                     end_date_value=0,
+                                     price_operation='',
+                                     price_value=0):
+        # Calculate the offset based on the page and limit
+        offset = (page - 1) * limit
+
+        conditions_dict = dict()
+
+        conditions_dict["offer_name_condition"] = f'offer_name = "{offer_name}"' if offer_name and offer_name.strip() else ''
+        conditions_dict["country_condition"] = f'location LIKE "{country}"%' if country and country.strip() else ''
+        conditions_dict["people_count_condition"] = f' people_count {people_count_operation} {people_count_value}' \
+            if (people_count_operation
+                and people_count_operation.strip()
+                and people_count_value > 0) else ''
+        conditions_dict["food_info_condition"] = f'food_info = "{food_info}"' if (
+                    food_info and food_info.strip()) else ''
+        conditions_dict["night_count_condition"] = f'night_count {night_count_operation} {night_count_value}' \
+            if (night_count_operation
+                and night_count_operation.strip()
+                and night_count_value > 0) else ''
+        conditions_dict["end_date_condition"] = f'start_date {end_date_operation}, {end_date_value}' \
+            if (end_date_operation
+                and end_date_operation.strip()
+                and end_date_value > 0) else ''
+        conditions_dict["start_date_condition"] = f'start_date {start_date_operation}, {start_date_value}' \
+            if (start_date_operation
+                and start_date_operation.strip()
+                and start_date_value > 0) else ''
+
+        conditions_dict["price_condition"] = f'price {price_operation} {price_value}' if (price_operation
+                                                                                          and price_operation.strip()
+                                                                                          and price_value > 0) else ''
+        query = "SELECT * FROM offer WHERE "
+
+        is_first_cond = True
+        for condition in conditions_dict.values():
+            if condition:
+                if not is_first_cond:
+                    query += 'AND ' + condition
+                    is_first_cond = False
+                else:
+                    query += condition
+
+        query += ' ORDER BY uniq_id DESC LIMIT ? OFFSET ?'
+        return self.__execute_query(query, limit, offset)
+        # self.cursor.execute(query, (limit, offset))
+        # rows = self.cursor.fetchall()
+        # records = []
+        # for row in rows:
+        #     offer_id = row[1]
+        #     query_links = '''
+        #                        SELECT * FROM offer_links WHERE offer_id = ?;
+        #                    '''
+        #     self.cursor.execute(query_links, (offer_id,))
+        #     links_row = self.cursor.fetchall()
+        #     links = []
+        #     for link in links_row:
+        #         links.append(link[2])
+        #     record = {
+        #         "offer_id": row[1],
+        #         "offer_name": row[2],
+        #         "offer_source": row[3],
+        #         "location": row[4],
+        #         "people_count": row[5],
+        #         "description": row[6],
+        #         "food_info": row[7],
+        #         "night_count": row[8],
+        #         "start_date": row[9],
+        #         "end_date": row[10],
+        #         "links": links
+        #     }
+        #     records.append(record)
+        # return json.dumps(records)
 
     def close_connection(self):
         self.conn.close()
--- src/restapi/example.py
+++ src/restapi/example.py
@@ -1,7 +1,7 @@
 from db_controller import DbController
 
 if __name__ == '__main__':
-    db_controller = DbController("my_database.db")
+    db_controller = DbController("..\my_database.db")
     # db_controller.add_data(
     #     offer_id=5555,
     #     offer_name="Paris",
@@ -15,5 +15,5 @@ if __name__ == '__main__':
     #    end_date="15.10.2023",
     #     links=["link1", "link2", "link2"],
     # )
-    json_records = db_controller.get_all_records_as_json(5, 1)
+    json_records = db_controller.get_filtered_records_as_json(5, 1, food_info='Все включено')
     print(json_records)
